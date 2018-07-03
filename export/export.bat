3e1ab07d564ffd13e74aa5b93ac43f019e58adab
Chavalit <chavalit,kow@gmail.com>
2018-07-03 13:11:27 +0700

    travel

---
+++ export/export.bat
@@ -0,0 +1,4 @@
+cd C:\Program Files\MongoDB\Server\4.0\bin
+mongodump -d mean-crud -o C:\Users\VRU_COMPUTER\Desktop
+echo "done"
+pause
---
+++ export/import.bat
@@ -0,0 +1,4 @@
+cd C:\Program Files\MongoDB\Server\4.0\bin
+mongorestore -d mean-crud C:\Users\VRU_COMPUTER\Desktop\mean-crud
+echo "done"
+pause
